1a69a0877ee107d520e452699dff4b214c106e4d
sanshiro1110 <66013439+sanshiro1110@users.noreply.github.com>
2020-11-12 22:55:11 +0900

    Add files via upload

---
+++ portfolio site/js/main.js
@@ -0,0 +1,54 @@
+const menuButton = document.querySelector('#menu');
+const menuContent = document.querySelector('#menu-content');
+const goBackButton = document.querySelector('#go-back-button');
+
+menuButton.addEventListener('click', () => {
+  menuContent.classList.toggle('open');
+  menuButton.classList.toggle('open');
+});
+goBackButton.addEventListener('click', () => {
+  menuContent.classList.remove('open');
+  menuButton.classList.remove('open');
+});
+
+//menuの項目クリック時にwindowを移動させる
+const toProfile = document.querySelectorAll('#to-profile');
+const profileHeight = document.querySelector('.profile').offsetTop;
+const toSkills = document.querySelectorAll('#to-skills');
+const skillsHeight = document.querySelector('.skills').offsetTop;
+const toWorks = document.querySelectorAll('#to-works');
+const worksHeight = document.querySelector('.works').offsetTop;
+const toContact = document.querySelectorAll('#to-contact');
+const contactHeight = document.querySelector('.contact').offsetTop;
+
+for(let i = 0; i < toProfile.length; i ++) {
+
+  toProfile[i].addEventListener('click', () => {
+    window.scrollTo({
+      top: profileHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
+  toSkills[i].addEventListener('click', () => {
+    window.scrollTo({
+      top: skillsHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
+  toWorks[i].addEventListener('click', () => {
+    window.scrollTo({
+      top: worksHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
+  toContact[i].addEventListener('click', () => {
+    window.scrollTo({
+      top: contactHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
+  });
+}
